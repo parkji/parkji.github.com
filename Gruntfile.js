ae763b3719837e084347efed6c5946d88bce0f58
Ben Parker <ben@mail24.co.uk>
2013-08-11 20:17:12 +0100

    Adds grunt task to build and serve site

---
+++ Gruntfile.js
@@ -0,0 +1,36 @@
+/**
+ * Uses grunt-shell to build and serve website using jekyll
+ * The watch task then runs these on file change
+ */
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+        shell: {
+            jekyllBuild: {
+                command: 'jekyll build'
+            },
+            jekyllServe: {
+                command: 'jekyll serve'
+            }
+        },
+        watch: {
+            files: [
+                '_includes/*.html',
+                '_layouts/*.html',
+                '_posts/*.markdown',
+                '_config.yml',
+                'index.html'
+            ],
+            tasks: ['shell:jekyllBuild', 'shell:jekyllServe'],
+            options: {
+                interrupt: true,
+                atBegin: true
+            }
+        }
+    });
+
+    grunt.loadNpmTasks('grunt-shell');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default', ['shell']);
+};
